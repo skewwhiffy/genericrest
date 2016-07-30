7b1ca99514b9914efcdfc9567e5354aa0e63b241
Kenny Hung <kennysubs+github@outlook.com>
2016-07-30 08:47:39 +0100

    Set up continuous testing.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let gulp = require("gulp");
+let mocha = require("gulp-mocha");
+let util = require("gulp-util");
+let nodemon = require("gulp-nodemon");
+
+gulp.task("run", function () {
+    return gulp.src(['**/*Tests.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', util.log);
+});
+
+gulp.task("watch-run", function () {
+    gulp.watch(['**/*.js'], ['run']);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+Run tests once with `gulp run`.
+
+Run tests continuously with `npm test` or `gulp watch-run`.
